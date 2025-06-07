f5dc7d619dc60592a864347be19fcb56f3861dcc
vijaythapakdata <vijaythapak24@gmail.com>
2025-06-07 18:47:58 +0530

    final updates

--- src/webparts/crudOperation/components/CrudOperation.tsx
+++ src/webparts/crudOperation/components/CrudOperation.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import type { ICrudOperationProps } from './ICrudOperationProps';
 import {spfi,SPFI,SPFx} from "@pnp/sp/presets/all";
+import { DefaultButton, DetailsList, Dialog, DialogFooter, DialogType, IconButton, PrimaryButton, SelectionMode, TextField } from '@fluentui/react';
 interface ICrudOperationState{
   Title:string;
   EmailAddress:string;
@@ -19,6 +20,10 @@ const  CrudOperation=(props:ICrudOperationProps):React.ReactElement=>{
   const[newTitle,setNewTitle]=React.useState<string>('');
   const[newEmail,setNewEmail]=React.useState<string>('');
   const[isAddHidden,setIsAddHidden]=React.useState<boolean>(true);
+  const[currentId,setCurrentId]=React.useState<number|any>();
+  const[editHidden,setIsEditHidden]=React.useState<boolean>(true);
+   const[editTitle,setEditTitle]=React.useState<string>('');
+  const[editEmail,setEditEmail]=React.useState<string>('');
 
   React.useEffect(()=>{
     _getListItems();
@@ -33,6 +38,7 @@ setStates(_getList.map((each:ICrudOperationState)=>({
   id:each.Id
 })));
 
+console.log("item saved successfully");
     }
     catch(err){
 console.log(err);
@@ -55,7 +61,7 @@ const _createListItems=async()=>{
   try{
 await _list.items.add({
   Title:newTitle,
-  EmailAddress:setNewEmail
+  EmailAddress:newEmail
 });
 //close the modal
 setIsAddHidden(true);
@@ -67,9 +73,184 @@ console.log(err);
   finally{
 setIsAddHidden(true)
   }
+
+}
+//opent dialog
+const openDialog=(id:number)=>{
+  setCurrentId(id);
+  //this function willopen the dialog while clicking editicon
+  setIsEditHidden(false);
+  const items:ICrud|undefined=states.find((each:any)=>each.id===id);
+  if(items){
+    setEditEmail(items.emailaddress);
+    setEditTitle(items.title);
+  }
+}
+const handleEditTitle=(event:React.ChangeEvent<HTMLInputElement>)=>{
+ 
+  setEditTitle(event.target.value);
+}
+const handleEditEmail=(event:React.ChangeEvent<HTMLInputElement>)=>{
+  setEditEmail(event.target.value);
+}
+const _updateItems=async()=>{
+  const list=_sp.web.lists.getByTitle(props.ListName);
+  try{
+    await list.items.getById(currentId).update({
+      Title:editTitle,
+      EmailAddress:editEmail
+    });
+    //close the dialog
+    setIsEditHidden(true);
+    setReload(!reload);
+  }
+  catch(err){
+    console.log(err);
+
+  }
+  finally{
+    setIsEditHidden(true);
+  }
+}
+//delete item
+const _deleteItems=async(id:number)=>{
+  const list=_sp.web.lists.getByTitle(props.ListName);
+  try{
+    await list.items.getById(id).delete();
+    setReload(!reload);
+    
+  }
+  catch(err){
+    console.log(err);
+  }
 }
   return(
-    <></>
+    <>
+    <DetailsList
+    items={states||[]}
+    columns={[
+      {
+        key:"Name",
+        name:"Name",
+        fieldName:"Title",
+        isResizable:true,
+        minWidth:200,
+        onRender:(item:ICrud)=><div>{item.title}</div>
+      },
+      {
+        key:"Email Address",
+        name:"Email Address",
+        fieldName:"EmailAddress",
+        minWidth:200,
+        isResizable:true,
+        onRender:(item:ICrud)=><div>{item.emailaddress}</div>
+      },
+      {
+        key:"Action Column",
+        name:"Actions",
+        fieldName:"actions",
+        minWidth:200,
+        isResizable:true,
+        onRender:(item:ICrud)=>(
+          <div>
+            <IconButton
+            iconProps={{iconName:'edit'}}
+            onClick={()=>openDialog(item.id)}
+            title='Edit'
+            ariaLabel='Edit'
+            />
+              <IconButton
+            iconProps={{iconName:'delete'}}
+            onClick={()=>_deleteItems(item.id)}
+            title='Delete'
+            ariaLabel='Delete'
+            />
+          </div>
+        )
+      }
+    ]}
+    selectionMode={SelectionMode.none}
+    />
+    <Dialog
+    hidden={editHidden}
+    onDismiss={()=>setIsEditHidden(true)}
+    dialogContentProps={{
+      type:DialogType.largeHeader,
+      title:'Edit Form'
+    }}
+    >
+
+<div>
+  <TextField
+  label='Name'
+  value={editTitle}
+  onChange={handleEditTitle}
+  />
+<TextField
+  label='Email Address'
+  value={editEmail}
+  onChange={handleEditEmail}
+  />
+</div>
+<DialogFooter>
+  <PrimaryButton
+  text='Save'
+  iconProps={{iconName:'save'}}
+  onClick={()=>_updateItems()}
+
+  />
+  <DefaultButton
+  text='Cancel'
+  iconProps={{iconName:'cancel'}}
+  onClick={()=>setIsEditHidden(true)}
+  
+  />
+</DialogFooter>
+    </Dialog>
+
+
+  <div>
+    <PrimaryButton text='Add Item'
+    iconProps={{iconName:'add'}}
+    onClick={()=>setIsAddHidden(false)}
+    />
+    </div>
+    <Dialog
+    hidden={isAddHidden}
+    onDismiss={()=>setIsAddHidden(true)}
+    dialogContentProps={{
+      type:DialogType.largeHeader,
+      title:'New Form'
+    }}
+    >
+      <div>
+  <TextField
+  label='Name'
+  value={newTitle}
+  onChange={handleNewTitle}
+  />
+<TextField
+  label='Email Address'
+  value={newEmail}
+  onChange={handleNewEmail}
+  />
+</div>
+<DialogFooter>
+  <PrimaryButton
+  text='Save'
+  iconProps={{iconName:'save'}}
+  onClick={()=>_createListItems()}
+
+  />
+  <DefaultButton
+  text='Cancel'
+  iconProps={{iconName:'cancel'}}
+  onClick={()=>setIsAddHidden(true)}
+  
+  />
+</DialogFooter>
+      </Dialog>
+    </>
   )
 }
 export default  CrudOperation;
